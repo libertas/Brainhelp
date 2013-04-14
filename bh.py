351bc7821e2c62de06c5f42c7bdee91a789adc2f
Libertas <horizonvei@gmail.com>
2013-04-14 13:14:28 +0800

    bh can now recognise .bh .bf .b .c

--- bh.py
+++ bh.py
@@ -14,15 +14,28 @@ except:
 
 cout=0
 if bh.endswith(".bh"):
-	system("brainhelp "+bh+" "+bh+".bf")
+	ending=".bf"
+	if elf.endswith(".b"):
+		ending=".b"
+	system("brainhelp "+bh+" "+bh[:-3]+ending)
+	if elf.endswith(".bf") or elf.endswith(".b"):
+		exit(0)
 	cout=1
+	bh=bh[:-3]
 	bh+=".bf"
 	
 if bh.endswith(".bf") or bh.endswith(".b"):
-	system("awib <"+bh+" >"+bh+".c")
+	if bh.endswith(".bf"):
+		bh_c=-3
+	elif bh.endswith(".b"):
+		bh_c=-2
+	system("awibh <"+bh+" >"+bh[:bh_c]+".c")
 	if cout==1:
 		system("rm "+bh)
+	if elf.endswith(".c"):
+		exit(0)
 	cout=2
+	bh=bh[:bh_c]
 	bh+=".c"
 
 if bh.endswith(".c"):
